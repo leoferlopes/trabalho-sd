d7ef4d298d9ae982d82f92531bd43e232c28e230
Leonardo Lopes <leonardo_lopes@id.uff.br>
2016-07-22 20:28:49 -0300

    Get Started Baixei um exemplo de comunicação em anel e criei um projeto Netbeans

---
+++ BarbeiroDorminhoco/build/Debug/OpenMPI-Linux/main.o.d
@@ -0,0 +1,6 @@
+build/Debug/OpenMPI-Linux/main.o: main.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
