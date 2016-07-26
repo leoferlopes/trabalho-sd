f60c0ea5e6144ad6202415f4e4497ec3c4b92c56
Leonardo Lopes <leonardo_lopes@id.uff.br>
2016-07-26 19:53:45 -0300

    Até agora

--- BarbeiroDorminhoco/main.c
+++ BarbeiroDorminhoco/main.c
@@ -10,9 +10,56 @@
 #include <stdio.h>
 #include "mpi.h"
 
-int main(int argc, char *argv[])
-{
-    int rank, size, next, prev, message, tag = 201;
+#define ID_COORDENADOR 0;
+#define ID_BARBEIRO 1;
+
+#define CADEIRAS 6;
+
+#define TAG_RESPOSTA 200;
+#define TAG_PEDIDO 201;
+
+#define TRUE 1;
+#define FALSE 0;
+
+int tagResposta = TAG_RESPOSTA;
+int tagPedido = TAG_PEDIDO;
+int idCoordenador = ID_COORDENADOR;
+int idBarbeiro = ID_BARBEIRO;
+
+int rank, size;
+
+void coordenador() {
+    int processo;
+    int barbeiroLivre = TRUE;
+    while (1) {
+        MPI_Recv(&processo, 1, MPI_INT, MPI_ANY_SOURCE, tagPedido, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+        // testa se a mensagem é do barbeiro
+        if(processo == idBarbeiro){
+            // barbeiro avisa que está livre
+            // manda uma ordem de serviço para ele
+        }
+        
+        // Testa se tem espaço na fila
+        if (0) {
+            // Enfilera o pedido do processo
+            // ou já manda para o barbeiro se o barbeiro estiver livre
+        } else {
+            int message = FALSE;
+            MPI_Send(&message, 1, MPI_INT, processo, tagResposta, MPI_COMM_WORLD);
+        }
+    }
+}
+
+void barbeiro() {
+
+}
+
+void cliente() {
+
+}
+
+int main(int argc, char *argv[]) {
 
     /* Start up MPI */
 
@@ -20,59 +67,17 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    /* Calculate the rank of the next process in the ring.  Use the
-       modulus operator so that the last process "wraps around" to
-       rank zero. */
-
-    next = (rank + 1) % size;
-    prev = (rank + size - 1) % size;
-
-    /* If we are the "master" process (i.e., MPI_COMM_WORLD rank 0),
-       put the number of times to go around the ring in the
-       message. */
-
-    if (0 == rank) {
-        message = 10;
-
-        printf("Process 0 sending %d to %d, tag %d (%d processes in ring)\n",
-               message, next, tag, size);
-        MPI_Send(&message, 1, MPI_INT, next, tag, MPI_COMM_WORLD);
-        printf("Process 0 sent to %d\n", next);
-    }
-
-    /* Pass the message around the ring.  The exit mechanism works as
-       follows: the message (a positive integer) is passed around the
-       ring.  Each time it passes rank 0, it is decremented.  When
-       each processes receives a message containing a 0 value, it
-       passes the message on to the next process and then quits.  By
-       passing the 0 message first, every process gets the 0 message
-       and can quit normally. */
-
-    while (1) {
-        MPI_Recv(&message, 1, MPI_INT, prev, tag, MPI_COMM_WORLD,
-                 MPI_STATUS_IGNORE);
-
-        if (0 == rank) {
-            --message;
-            printf("Process 0 decremented value: %d\n", message);
-        }
-
-        MPI_Send(&message, 1, MPI_INT, next, tag, MPI_COMM_WORLD);
-        if (0 == message) {
-            printf("Process %d exiting\n", rank);
+    switch (rank) {
+        case idCoordenador:
+            coordenador();
             break;
-        }
+        case idBarbeiro:
+            barbeiro();
+            break;
+        default:
+            cliente();
+            break;
     }
-
-    /* The last process does one extra send to process 0, which needs
-       to be received before the program can exit */
-
-    if (0 == rank) {
-        MPI_Recv(&message, 1, MPI_INT, prev, tag, MPI_COMM_WORLD,
-                 MPI_STATUS_IGNORE);
-    }
-
-    /* All done */
 
     MPI_Finalize();
     return 0;
